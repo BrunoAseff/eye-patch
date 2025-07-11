a312d4402624abeccb47c897c503daf0ece6b265
Bruno Aseff <brunoaseff2@gmail.com>
2025-07-10 21:31:01 -0300

    feat: update infra stack with correct info

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+  const alertTopic = new sns.Topic(this, 'EyePatchAlerts');
+ 
+  const alertEmail = this.node.tryGetContext("alertEmail");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  alertTopic.addSubscription(
+    new subs.EmailSubscription(alertEmail)
+  );
+
+
+   const lambdaFunction = new lambda.Function(this, 'EyePatchFunction', {
+    runtime: lambda.Runtime.NODEJS_18_X,
+    code: lambda.Code.fromAsset('../lambda'),
+    handler: 'index.handler',
+    environment: {
+        TOPIC_ARN: alertTopic.topicArn, 
+      },
+
+    });
+
+  alertTopic.grantPublish(lambdaFunction);
+
   }
 }
