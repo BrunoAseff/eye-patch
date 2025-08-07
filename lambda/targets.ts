9908b2fc579b13733b30ce2c8194db30eea905a1
Bruno Aseff <brunoaseff2@gmail.com>
2025-08-07 12:07:12 -0300

    feat: add label to targets

--- lambda/targets.ts
+++ lambda/targets.ts
@@ -2,21 +2,25 @@ import type { Target } from './types';
 
 export const targets: Target[] = [
   {
+    label: 'flaira-api',
     type: 'api',
     name: 'Flaira',
     url: 'https://api.flaira.net/status',
   },
   {
+    label: 'flaira-web',
     type: 'web',
     name: 'Flaira',
     url: 'https://flaira.net',
   },
   {
+    label: 'portfolio',
     type: 'web',
     name: 'Portfólio',
     url: 'https://brunoaseff.com.br',
   },
   {
+    label: 'nova',
     type: 'web',
     name: 'Nova',
     url: 'https://novaspaces.io',
